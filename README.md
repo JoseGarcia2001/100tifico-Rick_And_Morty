033e180582d99fb56733c1cad030e2e06428360d
Jose Garcia <jagarcia7655@gmail.com>
2021-05-03 20:43:20 -0500

    Cargando build

--- README.md
+++
@@ -1,2 +0,0 @@
-# 100tifico-Rick_And_Morty
-Este es un proyecto para crear una SPA con Javascript Vanilla
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { routes } from "./routes/routes";
-import Router from "./routes/Router";
-
-const container = document.getElementById("content");
-
-const router = new Router(routes, container);
-
-window.addEventListener("load", router.renderRoute);
-onhashchange = router.renderRoute;
